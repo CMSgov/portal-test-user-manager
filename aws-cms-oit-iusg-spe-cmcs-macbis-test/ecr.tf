b842f0e23fd3e0ece5d51054ce0744c2d6aa6135
ben-harvey <benjaminriderharvey@gmail.com>
2022-02-16 09:29:41 -0500

    changes for review

--- aws-cms-oit-iusg-spe-cmcs-macbis-test/ecr.tf
+++ aws-cms-oit-iusg-spe-cmcs-macbis-test/ecr.tf
@@ -1,8 +1,11 @@
 module "cross_account_ecr" {
   source = "../cross-account-ecr"
 
-  principal_arns = ["arn:aws:iam::037370603820:root", "arn:aws:iam::741306476019:root"] // macbis-dev, MACFin
-  repo_name  = "password-rotation"
+  principal_arns = [
+    "arn:aws:iam::037370603820:root", # macbis-dev
+    "arn:aws:iam::741306476019:root"  # MACFin
+  ]
+  repo_name = "password-rotation"
 }
 
 output "cross_account_ecr_outputs" {
